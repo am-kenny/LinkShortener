d6632a06d0010c58cb33cd20c842df638e8edd6a
Andrii Prykhodko <a.v.prykhodko25@gmail.com>
2023-10-26 01:57:40 +0200

    Change python version in Dockerfile to 3.10, add test user on db initialization

--- mongo-init.js
+++ mongo-init.js
@@ -5,6 +5,14 @@ db = db.getSiblingDB('link_shortener_db');
 // Create the 'links' collection
 db.createCollection('links');
 db.createCollection('telegram_users');
-db.createCollection('users');
 db.createCollection('redirects');
 
+db.createCollection('users');
+db.users.insert({
+    _id: ObjectId(),
+    username: 'johndoe',
+    full_name: 'John Doe',
+    email: 'johndoe@example.com',
+    hashed_password: '5ebe2294ecd0e0f08eab7690d2a6ee69',
+    disabled: false
+})
